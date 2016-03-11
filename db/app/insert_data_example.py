35cfac30649488ac12ea4f2d043b3efda1fe3f39
giwa <ugw.gi.world@gmail.com>
2016-03-11 17:48:45 +0900

    add sql generation example for inserting data

---
+++ db/app/insert_data_example.py
@@ -0,0 +1,36 @@
+# Sample for SQL generation from data.csv
+# You can follow this example and should write
+# the rest of code to insert data to database
+
+INSERT_TMP="""\
+INSERT INTO users (name, bday, age, state)
+VALUES
+{}
+"""
+
+def convert_values(data):
+    """
+    Convert list into VALUES format
+
+    (name, bday, age, state)
+    """
+    return '("{}", "{}", {}, "{}")'.format(*data)
+
+data = []
+with open('./data.csv') as f:
+    for line in f:
+        data.append(line.strip().split(','))
+
+sql = INSERT_TMP.format(",\n".join(convert_values(d) for d in data))
+
+"""
+example of output
+
+INSERT INTO users (name, bday, age, state)
+VALUES
+("Bettye Purdy", "2001-05-21", 15, "Maine"),
+("Mrs. Elisabeth Wilderman MD", "1999-01-21", 17, "Pennsylvania"),
+("Edgar Kreiger", "1973-11-11", 43, "Vermont"),
+("Saverio McCullough", "2007-10-13", 9, "Idaho"),
+...
+"""
